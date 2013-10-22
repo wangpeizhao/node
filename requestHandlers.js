05e5f6f6cdfa83e3d59f4f574904ac5f6d3baf3e
wangpeizhao <342823274@qq.com>
2013-10-22 16:45:19 +0800

    add the js files
    add the js files

---
+++ requestHandlers.js
@@ -0,0 +1,35 @@
+﻿var exec = require("child_process").exec;
+
+function start(response){
+	console.log("this is a function of start is called");
+	function sleep(milliSeconds){
+		var the_time = new Date().getTime();
+		while(new Date().getTime()<the_time + milliSeconds);
+	}
+	//sleep(5000);
+	/*exec("ls -lah",function (error, stdout, stderr) {
+		response.writeHead(200, {"Content-Type": "text/plain"});
+		response.write(stdout);
+		response.end();
+	});*/
+	var result = 0;
+	var i = 0;
+	for(i=0;i<10000000000;i++){
+		result = result + i;
+	}
+	response.writeHead(200, {"Content-Type": "text/html"});
+	response.write('<p>The result is :'+result+'</p>');
+	response.write('<font style="color:red">I wake up!('+(new Date())+')</font>');
+	response.end();
+	console.log("I wake up!");
+}
+
+function upload(response){
+	console.log("this is a function of upload is called");
+	response.writeHead(200, {"Content-Type": "text/plain"});
+	response.write("Hello Upload");
+	response.end();
+}
+
+exports.start = start;
+exports.upload = upload;
---
+++ server.js
@@ -0,0 +1,23 @@
+﻿var http = require("http");
+var url = require("url");
+
+function start(route,handle) {
+  function onRequest(request, response) {
+    var pathname = url.parse(request.url).pathname;
+	console.log(pathname);
+    console.log("Request for " + pathname + " received.");
+
+	route(handle,pathname,response);
+
+    //response.writeHead(200, {"Content-Type": "text/html"});
+    //response.write("Hello World");
+	//response.write('<p><input type="text" name="txt1" value="'+pathname+'"></p>');
+	//response.write('<p><input type="text" name="txt2" value="'+pathname+'"></p>');
+    //response.end();
+  }
+
+  http.createServer(onRequest).listen(8888);
+  console.log("Server has started.");
+}
+
+exports.start = start;
